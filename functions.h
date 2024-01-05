3ca4afc9103d77442185b2ee0ab178d7ad29841c
BrendanAnalikwu <34724641+BrendanAnalikwu@users.noreply.github.com>
2024-01-05 11:56:24 +0100

    Added scaling to FourierSum

--- functions.h
+++ functions.h
@@ -15,10 +15,13 @@ class FourierSum
 private:
     DoubleVector coef_x_c, coef_y_c, coef_x_s, coef_y_s;
     DoubleVector indices_x_c, indices_y_c, indices_x_s, indices_y_s;
+    double scaling_min, scaling_max, scaling, intercept;
 public:
 
     std::map<double, double> map_x_c, map_y_c, map_x_s, map_y_s;
+
     FourierSum() = default;
+
     FourierSum(const std::string& name, const ParamFile& pf)
     {
         DataFormatHandler DFH;
@@ -30,6 +33,8 @@ public:
         DFH.insert(name + "_x_s", &coef_x_s);
         DFH.insert(name + "_i_y_s", &indices_y_s);
         DFH.insert(name + "_y_s", &coef_y_s);
+        DFH.insert(name + "_min", &scaling_min);
+        DFH.insert(name + "_max", &scaling_max);
         FileScanner FS(DFH);
         FS.NoComplain();
         FS.readfile(pf, "Coefficients");
@@ -37,6 +42,18 @@ public:
         assert(indices_y_c.size() == coef_y_c.size());
         assert(indices_x_s.size() == coef_x_s.size());
         assert(indices_y_s.size() == coef_y_s.size());
+
+        double sigma_x = 0;
+        for (const auto& c: coef_x_c) sigma_x += std::abs(c);
+        for (const auto& c: coef_x_s) sigma_x += std::abs(c);
+        double sigma_y = 0;
+        for (const auto& c: coef_y_c) sigma_y += std::abs(c);
+        for (const auto& c: coef_y_s) sigma_y += std::abs(c);
+        double sigma = sigma_x * sigma_y;
+
+        double range = scaling_max - scaling_min;
+        intercept = range / 2. + scaling_min;
+        scaling = range / (2. * sigma);
 
         fill_mapping(map_x_c, indices_x_c, coef_x_c);
         fill_mapping(map_y_c, indices_y_c, coef_y_c);
@@ -48,6 +65,7 @@ public:
     {
         return fourier_sum(map_x_c, map_x_s, x) * fourier_sum(map_y_c, map_y_s, y);
     }
+
     double inline operator()(const Vertex2d& v) const { return (*this)(v.x(), v.y()); }
 
     static void fill_mapping(std::map<double, double>& mapping, const DoubleVector& indices, const DoubleVector& coef)
@@ -55,12 +73,13 @@ public:
         for (int i = 0; i < indices.size(); ++i) { mapping[indices[i]] = coef[i]; }
     }
 
-    static double inline fourier_sum(const std::map<double, double>& coef_cos, const std::map<double, double>& coef_sin, double x)
+    double inline
+    fourier_sum(const std::map<double, double>& coef_cos, const std::map<double, double>& coef_sin, double x) const
     {
         double res = 0.;
         for (auto it: coef_cos) { res += it.second * cos(it.first * x); }
         for (auto it: coef_sin) { res += it.second * sin(it.first * x); }
-        return res;
+        return res * scaling + intercept;
     }
 
 };
