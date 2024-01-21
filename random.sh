c3188b5c0c4b747b2bbd4a2871702f9593974f39
BrendanAnalikwu <34724641+BrendanAnalikwu@users.noreply.github.com>
2024-01-21 13:19:41 +0100

    Presets for dataset 'big'

--- random.sh
+++ random.sh
@@ -6,10 +6,10 @@
 #SBATCH --time=30 # default in minutes
 #SBATCH --account=education-eemcs-msc-am
 #SBATCH --ntasks=1
-#SBATCH --output="Results/slurm-%j.out"
+#SBATCH --output="Results/big/slurm-%j.out"
 
 ID="$SLURM_JOB_ID"
-dir="/scratch/boanalikwu/Results/$ID"
+dir="/scratch/boanalikwu/Results/big/$ID"
 
 mkdir -p $dir
 echo '//Block Coefficients' > $dir/coef.param
@@ -20,13 +20,13 @@ do
  do
  for cs in 'c' 's'
  do
-  n=$((1 + $RANDOM % 4))
+  n=$((1 + $RANDOM % 2))
   ind=""
   coef=""
 
   for ((i=1; i<=$n; i++))
   do
-   ind+=" $(echo "scale=8; e($RANDOM/32768*l(512)+l(3.14159265))" | bc -l )"
+   ind+=" $(echo "scale=8; e($RANDOM/32768*l(16)+l(3.14159265))" | bc -l )"
    coef+=" $(echo "scale=8; 2*$RANDOM/32768 -1" | bc )"
   done
   echo "$var"_i_"$xy"_"$cs" $n $ind >> $dir/coef.param
@@ -55,17 +55,17 @@ echo H_max "$(echo "scale=8; .35*$RANDOM/32768 + .35" | bc )" >> $dir/coef.param
 echo A_min "$(echo "scale=8; .7*$RANDOM/32768 - .1" | bc )" >> $dir/coef.param
 echo A_max "$(echo "scale=8; .35*$RANDOM/32768 + .8" | bc )" >> $dir/coef.param
 
-echo u00_min "$(echo "scale=12; .0002*$RANDOM/32768 - .0002" | bc )" >> $dir/coef.param
-echo u00_max "$(echo "scale=12; .0002*$RANDOM/32768" | bc )" >> $dir/coef.param
+echo u00_min "$(echo "scale=12; .0001*$RANDOM/32768 - .0002" | bc )" >> $dir/coef.param
+echo u00_max "$(echo "scale=12; .0001*$RANDOM/32768 + .0001" | bc )" >> $dir/coef.param
 
-echo u01_min "$(echo "scale=12; .0002*$RANDOM/32768 - .0002" | bc )" >> $dir/coef.param
-echo u01_max "$(echo "scale=12; .0002*$RANDOM/32768" | bc )" >> $dir/coef.param
+echo u01_min "$(echo "scale=12; .0001*$RANDOM/32768 - .0002" | bc )" >> $dir/coef.param
+echo u01_max "$(echo "scale=12; .0001*$RANDOM/32768 + .0001" | bc )" >> $dir/coef.param
 
-echo Ox_min "$(echo "scale=13; .000005*$RANDOM/32768 - .00001" | bc )" >> $dir/coef.param
-echo Ox_max "$(echo "scale=13; .000005*$RANDOM/32768 + .000005" | bc )" >> $dir/coef.param
+echo Ox_min "$(echo "scale=13; .00001*$RANDOM/32768 - .000015" | bc )" >> $dir/coef.param
+echo Ox_max "$(echo "scale=13; .00001*$RANDOM/32768 + .000005" | bc )" >> $dir/coef.param
 
-echo Oy_min "$(echo "scale=13; .000005*$RANDOM/32768 - .00001" | bc )" >> $dir/coef.param
-echo Oy_max "$(echo "scale=13; .000005*$RANDOM/32768 + .000005" | bc )" >> $dir/coef.param
+echo Oy_min "$(echo "scale=13; .00001*$RANDOM/32768 - .000015" | bc )" >> $dir/coef.param
+echo Oy_max "$(echo "scale=13; .00001*$RANDOM/32768 + .000005" | bc )" >> $dir/coef.param
 
 
 
@@ -74,7 +74,7 @@ echo 'W_mx '      "$(echo "scale=8; .4 * $RANDOM/32768 + .05" | bc)" >> $dir/coe
 echo 'W_my '      "$(echo "scale=8; .4 * $RANDOM/32768 + .05" | bc)" >> $dir/coef.param
 echo 'W_vmax'     "$(echo "scale=8; .015 * $RANDOM/32768 + .005" | bc)" >> $dir/coef.param
 echo 'W_alpha '   "$(echo "scale=8; .523598776*$RANDOM/32768+1.04719755" | bc)" >> $dir/coef.param
-echo 'W_r0 '      "$(echo "scale=8; .09 * $RANDOM/32768 + .01" | bc)" >> $dir/coef.param
+echo 'W_r0 '      "$(echo "scale=8; .1 * $RANDOM/32768 + .05" | bc)" >> $dir/coef.param
 echo 'W_cyclone ' "$(($RANDOM % 2))" >> $dir/coef.param
 
 echo '//Block Nix' >> $dir/coef.param
